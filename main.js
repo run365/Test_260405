0175a149bb44b67d0cf74b092448f9ed84667b0a
junggupil <junggupil@gmail.com>
2026-04-05 04:05:01 +0000

    Upload current project code to GitHub

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// Add JS here
+const drawButton = document.getElementById('draw');
+const numbersContainer = document.getElementById('numbers');
+
+drawButton.addEventListener('click', () => {
+    numbersContainer.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+    for (const number of numbers) {
+        const numberDiv = document.createElement('div');
+        numberDiv.classList.add('number');
+        numberDiv.textContent = number;
+        numbersContainer.appendChild(numberDiv);
+    }
+});
